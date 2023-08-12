a0ae5f11a8b04caaf7c0107f63d8b16dced06eef
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-08-12 09:27:55 +0100

    feature: Create first examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, computed, signal } from '@angular/core';
+import { Component, OnInit, computed, signal, effect } from '@angular/core';
+import { Vehicle } from './models/vehicle';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { Component, OnInit, computed, signal } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'ng-signals';
+  quantity = signal<number>(1);
+  qtyAvailable = signal<number[]>([1,2,3,4,5,6]);
+  selectedVehicle = signal<Vehicle>({id: 1,
+    name: 'AT-AT', price: 19416.13});
+  vehicles = signal<Vehicle[]>([]);
+  exPrice = computed(() =>
+    this.selectedVehicle().price * this.quantity());
+  qtyEff = effect(() => console.log(this.quantity()));
+
+    constructor() {
+      effect(() => console.log(this.selectedVehicle()));
+    }
 
   x = signal(5);
   y = signal(3);
@@ -16,5 +29,15 @@ export class AppComponent implements OnInit{
     console.log(this.z());
     this.x.set(10);
     console.log(this.z());
+  }
+
+  onQuantitySelected(qty: number) {
+    this.quantity.set(qty);
+  }
+
+  onSomeEvent(qty: number) {
+    this.quantity.set(qty);
+    this.quantity.set(5);
+    this.quantity.set(42);
   }
 }
