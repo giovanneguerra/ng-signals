6e8a5fe1397ef15fd3d149875c2c4b2c55330ab7
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-08-12 09:50:57 +0100

    refactor: Organize template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,34 +10,22 @@ export class AppComponent implements OnInit{
   title = 'ng-signals';
   quantity = signal<number>(1);
   qtyAvailable = signal<number[]>([1,2,3,4,5,6]);
-  selectedVehicle = signal<Vehicle>({id: 1,
-    name: 'AT-AT', price: 19416.13});
+  selectedVehicle = signal<Vehicle>({id: 1, name: 'AT-AT', price: 10000});
   vehicles = signal<Vehicle[]>([]);
-  exPrice = computed(() =>
-    this.selectedVehicle().price * this.quantity());
-  qtyEff = effect(() => console.log(this.quantity()));
+  exPrice = computed(() => this.selectedVehicle().price * this.quantity());
+  qtyEff = effect(() => console.log("Latests quantity:", this.quantity()));
 
     constructor() {
-      effect(() => console.log(this.selectedVehicle()));
+      this.quantity.update(qty => qty *2);
+      this.selectedVehicle.mutate(v => v.price = v.price + (v.price *.2));
     }
 
-  x = signal(5);
-  y = signal(3);
-  z = computed(() => this.x() + this.y());
-
   ngOnInit(): void {
-    console.log(this.z());
-    this.x.set(10);
-    console.log(this.z());
   }
 
   onQuantitySelected(qty: number) {
     this.quantity.set(qty);
-  }
-
-  onSomeEvent(qty: number) {
-    this.quantity.set(qty);
-    this.quantity.set(5);
-    this.quantity.set(42);
+    // this.quantity.set(5);
+    // this.quantity.set(42);
   }
 }
